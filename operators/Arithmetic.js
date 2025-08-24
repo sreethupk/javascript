4924c4c25d92786f4dc8f43979660cb466ba1b62
sreethupk <sreethupkrishnadasan@gmail.com>
2025-08-24 20:12:10 +0530

    commited

--- operators/Arithmetic.js
+++ operators/Arithmetic.js
@@ -1,24 +1,28 @@
+//Additional
 let a =10;
 let b =2;
 
 console.log("a + b =",a + b);
 
-
+//substration
 let c =20;
 let d =10;
 
 console.log("c - d =",c - d);
 
+//multiplication
 let e =2;
 let g =6;
 
 console.log("e * g =",e * g);
 
+//modules
 let m =4;
 let n =6;
 
 console.log("m % n =",m % n);
 
+//divion
 let o =3;
 let h =3;
 
@@ -36,6 +40,7 @@ let s =5;
 console.log("s++ =",s++);
 console.log("after s++, s=", s);
 
+//decrement
 let x =3;
 console.log("x++ =",x--);
 console.log("after x--, x=", x);
